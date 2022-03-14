b1fbcf1ca8e38c746aaccc50e2f59fe6b4e8b954
Danilo Fernandes <dandan158@gmail.com>
2022-03-14 11:16:44 -0300

    captiulo2

--- src/main/kotlin/capitulo2/Main.kt
+++ src/main/kotlin/capitulo2/Main.kt
@@ -18,6 +18,21 @@ fun main() {
     val quadrado = Rectangle(100, 50)
 
     print(" eh quadrado? ${quadrado.isSquare}")
-[]
 
+    println(Color.BLUE.rgb())
+
+}
+
+//OS ENUMS CLASS NAO SAO LISTA DE VALORES, VC PODE DECLARAR PROPRIEDADES E METODOS EM CLASSES ENUM
+//a paralavra enum, é palavra reservada leve
+enum class Color(val r: Int, val g: Int, val b: Int) {
+    RED(255, 0, 0),
+    ORANGE(255, 0, 0),
+    YELLOW(255, 0, 0),
+    GREEN(255, 0, 0),
+    BLUE(255, 0, 0),
+    INDIGO(255, 0, 0),
+    VIOLET(255, 0, 0);
+
+    fun rgb() = (r * 256 + g) * 256 + b
 }
