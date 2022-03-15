852a917b1a67e6b11efeea8e01eb9e6889e648ed
Danilo Gonçalves <dangoncalves@pagseguro.com>
2022-03-15 14:52:28 -0300

    update colecoes

--- src/main/kotlin/capitulo2/colecoes.kt
+++ src/main/kotlin/capitulo2/colecoes.kt
@@ -16,4 +16,21 @@ fun main() {
     println(strings.maxOrNull())
 
 
+    //operador de espelhamento(spread opoerator) ele pega o array e desempacota ele, tornando cada elemento como um argumento para um metodo
+    val args: Array<String> = arrayOf("10", "100")
+
+    val listSpread = listOf("args", *args) //colocando o * asterisco, ele vai pegar o array, e desempacotar, colocando o varargs
+
+    println(listSpread)
+
+//    utilizando a funcao "to"
+    //modo usual
+    val map1 = mapOf(1.to("one"))
+    //usando a notacao "infix"
+    val map2 = mapOf(1 to "one")
+
+    //declaracao destruturante utilizando a funcao "to"
+    val(number, name) = 1 to "one"
+    println("number: $number  name: $name")
+
 }
