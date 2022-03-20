401eab4e2d1b0de58455270369a3185fb7a7df1a
dandan158@gmail.com <casa@15963>
2022-03-19 23:15:36 -0300

    add expressons_lambda.kt

---
+++ src/main/kotlin/captiulo5/expressons_lambda.kt
@@ -0,0 +1,69 @@
+package captiulo5
+
+data class Person(val name: String, val age: Int)
+
+val people = listOf(Person("Alice", 29), Person("Danilo", 34))
+
+
+//funcao de maxBy
+fun teste() {
+    //maxBY
+    people.maxByOrNull { it.age }
+
+    people.maxByOrNull { p: Person -> p.age }
+//    formas de fazer o lambda
+    //quando a lambda tem algum argumento, precisamos passar o parentes
+    //caso nao, podemos chamar direto as chave
+    people.maxByOrNull() {it.age}
+
+    //por exemplo um lambda com argumento, colocamos fora
+    people.joinToString(separator = " ") { it.name }
+
+    //outra forma com os parentes
+    people.joinToString(separator = " ", transform = { it.name} )
+
+    //em kotlin as variaveis dentro de um lambda podem ser mutaveis, diferente do ajva q precisa ser final
+    var counter: Int = 0
+    people.forEach {
+           counter++
+    }
+
+
+
+    //lambdas podemos fazer colocando as chaves
+    //colocar uma expre lambda em uma variavel
+
+    val sum = {x: Int, y: Int -> x + y}
+    println(sum(10, 6))
+
+    //outra forma de criar e executar uma expressao
+    //criar as chaves e colocar um parentes no final
+
+
+    //podemos utilizar a referencia de membro q é o dois pontos duplo
+    //para referencia as funcoes
+    val name = Person::name
+
+
+}
+
+//criar um lambda q executa uma funcao
+val action = {person: Person, message: String ->
+    sendMail(person, message)
+}
+
+fun sendMail(person: Person, email: String){
+    val referenciaVinculada = person::age
+    println("send $person, email $email")
+}
+
+val nextAction = ::sendMail//chamar uma refencia de membro de sem precisar passar a class
+
+
+fun salute() = println("salute")
+
+fun main() {
+    run(::salute)
+    //a funcao run executa uma funcao interna
+    run({println(45)})
+}
